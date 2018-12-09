39f34a7de47c33f6f1fe8da9fec21765949f2a72
AidinSh <aidin_shahmoradi@yahoo.com>
2018-12-09 23:53:48 +0330

    prototype-1
    adding assets to project .
    script for character .
    adding sound track .
    adding character animation .

---
+++ Journey of Life/Assets/Scripts/Character_Script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Script : MonoBehaviour {
+
+    private Rigidbody2D character;
+    private bool isDead = false;
+    private bool jumpReady = false;
+
+    public float upForce = 250f;
+
+	void Start ()
+    {
+        character = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if ( isDead == false)
+        {
+            if ( Input.GetKeyDown ("space") && jumpReady)
+            {
+                //character.velocity = Vector2.zero;
+                character.AddForce(new Vector2(0, upForce));
+                jumpReady = false;
+            }
+            if ( Input.GetKey ("right"))
+            {
+                character.AddForce(new Vector2(100, 0));
+            }
+        }
+	}
+
+    void OnCollisionEnter2D()
+    {
+        jumpReady = true;
+    }
+}
